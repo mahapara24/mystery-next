bc22f74448fd250af2ff366d83c0d80d68d2aedf
mahapara24 <mahanizamani12345678@gmail.com>
2024-04-22 22:21:47 +0500

    route upt

---
+++ src/app/api/auth/[nextauth].../options.ts
@@ -0,0 +1,53 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProviders from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProviders({
+      id: "crediantials",
+      name: "Crediantials",
+      credentials: {
+        username: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+          if (!user) {
+            throw new Error("No User found with this Email address");
+          }
+          if (!user.isVerified) {
+            throw new Error("Please verify your account before logging in");
+          }
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Incorrect Password");
+          }
+        } catch (err: any) {
+          throw new Error(err);
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString;
+      }
+    },
+  },
+};
